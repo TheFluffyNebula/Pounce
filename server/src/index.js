cd624100ef339293e4ab9d2c57288922e3dc9e23
Jason Yin <jyin23@ncsu.edu>
2025-08-01 23:19:40 -0400

    same index.js but from hearts repo directly

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,10 @@ import { Server } from "socket.io";
 import roomRoutes from "./routes/roomRoutes.js";
 import roomSockets from "./sockets/roomSockets.js";
 
+// const clientServerUrl = "http://localhost:5173";
+// Use Render's environment variable or fallback to localhost for dev
 const clientServerUrl = process.env.CLIENT_URL || "http://localhost:5173";
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -14,15 +17,21 @@ const io = new Server(server, {
 });
 
 app.use(cors({ origin: clientServerUrl }));
+// app.use(cors());
+// app.use(cors({ origin: clientServerUrl, optionsSuccessStatus: 200 }));
+
 app.use(express.json());
 
+// Serve the frontend (React build)
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "../client/dist")));
+
 
 app.use("/api/rooms", roomRoutes);
 
 roomSockets(io);
 
+// Handle any unknown routes and serve the frontend (React index.html)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
